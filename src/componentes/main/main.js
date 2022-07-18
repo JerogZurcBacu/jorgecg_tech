c5b79342465dd120d9b3ca56e32ec0a208c60cd3
JeroZurc <jorgecruzprof@gmail.com>
2022-07-18 18:09:04 -0500

    avance seccion proyectos

--- src/componentes/main/main.js
+++ src/componentes/main/main.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './style.scss';
 /* import Inicio from "../Inicio/inicio"; */
-import AboutMe from "../aboutMe/about";
+/* import AboutMe from "../aboutMe/about"; */
+import Proyectos from "../proyectos/proyectos";
 
 class Main extends React.Component {
     render () {
         return (
             <main className="contenedor-principal">
                 {/* <Inicio /> */}
-                <AboutMe />
+                {/* <AboutMe /> */}
+                <Proyectos />
             </main>
         )
     };
---
+++ src/componentes/proyectos/proyectos.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './estilos/style.scss';
+import Continuar from "../continuar/continuar";
+
+class Proyectos extends React.Component {
+    render() {
+        const settings = {
+          dots: false,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          adaptiveHeight: true
+        };
+        return (
+          <div className="contenedor-proyectos">
+            <h1>Proyectos</h1>
+            <Slider {...settings}>
+              <div className="contenedor-slider-pro">
+                <div className="img-proyectos" id="itgas" />
+                <a href="http://itgas.com.mx/" target="_blank" rel="noreferrer"><h2>ItGas - Sitio Web</h2></a>
+                <p>Diseño y desarrollo completo del sitio web de ItGas. 100% Responsivo.</p>
+                <p>Tecnologías:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                </ul>
+              </div>
+              <div>
+                <div className="img-proyectos" id="aguanat" />
+                <a href="https://www.aguanat.com/" target="_blank" rel="noreferrer"><h2>Aguanat - Sitio Web</h2></a>
+                <p>Diseño y desarrollo completo del sitio web de Aguanat. 100% Responsivo.</p>
+                <p>Tecnologías:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Boottrap</li>
+                </ul>
+              </div>
+              <div>
+                <div className="img-proyectos" id="silic" />
+                <a href="https://siliclab.mx/" target="_blank" rel="noreferrer"><h2>SilicLab - Ecommerce</h2></a>
+                <p>Diseño y desarrollo completo del ecommerce de SilicLab. 100% Responsivo.</p>
+                <p>Tecnologías:</p>
+                <ul>
+                    <li>Shopify</li>
+                </ul>
+              </div>
+              <div>
+                <div className="img-proyectos" id="jorge" />
+                <a href="https://jorgecg.tech/" target="_blank" rel="noreferrer"><h2>JorgeCG - Sitio Web</h2></a>
+                <p>Diseño y desarrollo completo del sitio web de JorgeCG. 100% Responsivo.</p>
+                <p>Tecnologías:</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>SASS</li>
+                    <li>Javacript</li>
+                    <li>React</li>
+                </ul>
+              </div>
+              <div>
+                <div className="img-proyectos" id="github" />
+                <a href="http://itgas.com.mx/" target="_blank" rel="noreferrer"><h2>Practicas - Sitio Web</h2></a>
+                <p>Practicas de cursos y diplomados almacenados en GitHub</p>
+                <p>Tecnologías:</p>
+                <ul>
+                    <li>Varios</li>
+                </ul>
+              </div>
+            </Slider>
+            <Continuar />
+          </div>
+        );
+      }
+    }
+
+export default Proyectos;
