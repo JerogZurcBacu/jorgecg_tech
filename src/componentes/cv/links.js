950db5c09753ba37184708f9f1ce8c3d55015a1f
JerogZurc <jorgecruzprof@gmail.com>
2022-07-22 19:13:24 -0500

    sección links-cv ok

--- src/componentes/cv/links.js
+++ src/componentes/cv/links.js
@@ -5,7 +5,36 @@ class Links extends React.Component {
     render () {
         return (
             <div className="contenedor-links">
-
+                <div className="sub-contenedpr-links">
+                    <hr />
+                    <div className="icono-titulo-links">
+                        <i class="fa-solid fa-person"></i>
+                        <h3 className="url">https://es.m.personal.com</h3>
+                    </div>
+                    <h3 className="titulo-link">Información personal</h3>
+                    <p>Información personal, intereses, gustos, pasatiempos, etc...</p>
+                    <hr />
+                </div>
+                <div className="sub-contenedpr-links">
+                    <hr />
+                    <div className="icono-titulo-links">
+                        <i class="fa-solid fa-graduation-cap"></i>
+                        <h3 className="url">https://www.estudios.com</h3>
+                    </div>
+                    <h3 className="titulo-link">Información Académica</h3>
+                    <p>Información académica, escolar, cursos, diplimados, etc...</p>
+                    <hr />
+                </div>
+                <div className="sub-contenedpr-links">
+                    <hr />
+                    <div className="icono-titulo-links">
+                        <i class="fa-solid fa-briefcase"></i>
+                        <h3 className="url">https://laboral.mx</h3>
+                    </div>
+                    <h3 className="titulo-link">Información Laboral</h3>
+                    <p>Información de proyectos y empleos anteriores.</p>
+                    <hr />
+                </div>
             </div>
         )
     }
